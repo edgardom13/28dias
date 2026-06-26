3cebd5dfdbeffae6a76746ac413fde3af98e460d
edgardom13 <edgardomontanoolucion@gmail.com>
2026-06-25 21:44:49 -0500

    Configurado para Vercel

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  url: 'https://twiihtyrqqxdtejpgwkj.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3aWlodHlycXF4ZHRlanBnd2tqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5Mzc0MjcsImV4cCI6MjA5NzUxMzQyN30.Vd_11dsPfcUWmuokq8mFNr46d8_COwokZlS6miwck7A'
+};
